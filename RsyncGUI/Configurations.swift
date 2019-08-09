e81cefcdb9f3212fa4b1808d2f1bb890a2849fb9
Thomas Evensen <thomeven@gmail.com>
2019-08-09 20:26:42 +0200

    Updates

--- RsyncGUI/Configurations.swift
+++ RsyncGUI/Configurations.swift
@@ -119,7 +119,7 @@ final class Configurations: ReloadTable, SetSchedules {
     /// Function returns all Configurations marked for backup.
     /// - returns : array of Configurations
     func getConfigurationsBatch() -> [Configuration] {
-        return self.configurations!.filter({return ($0.task == ViewControllerReference.shared.synchronize) && ($0.batch == "yes")})
+        return self.configurations!.filter({return ($0.task == ViewControllerReference.shared.synchronize) && ($0.batch == 1)})
     }
 
     /// Function computes arguments for rsync, either arguments for
@@ -236,10 +236,10 @@ final class Configurations: ReloadTable, SetSchedules {
     /// persisten store
     /// - parameter index: index of Configuration to toogle batch on/off
     func enabledisablebatch (_ index: Int) {
-        if self.configurations![index].batch == "yes" {
-            self.configurations![index].batch = "no"
+        if self.configurations![index].batch == 1 {
+            self.configurations![index].batch = 0
         } else {
-            self.configurations![index].batch = "yes"
+            self.configurations![index].batch = 1
         }
         self.storageapi!.saveConfigFromMemory()
         self.reloadtable(vcontroller: .vctabmain)
