323b98411b543802b04f2522762b7bb647210f63
Thomas Evensen <thomeven@gmail.com>
2019-08-26 12:12:57 +0200

    Cleanup

---
+++ RsyncGUI/extensionVCMainSingletask.swift
@@ -0,0 +1,83 @@
+//
+//  extensionVCMainSingletask.swift
+//  RsyncGUI
+//
+//  Created by Thomas Evensen on 26/08/2019.
+//  Copyright © 2019 Thomas Evensen. All rights reserved.
+//
+//  swiftlint:disable line_length
+
+import Foundation
+
+extension ViewControllerMain: SingleTaskProgress {
+
+    func presentViewProgress() {
+        globalMainQueue.async(execute: { () -> Void in
+            self.presentAsSheet(self.viewControllerProgress!)
+        })
+    }
+
+    func presentViewInformation(outputprocess: OutputProcess) {
+        self.outputprocess = outputprocess
+        if self.dynamicappend {
+            globalMainQueue.async(execute: { () -> Void in
+                self.mainTableView.reloadData()
+            })
+        } else {
+            globalMainQueue.async(execute: { () -> Void in
+                self.presentAsSheet(self.viewControllerInformation!)
+            })
+        }
+    }
+
+    func terminateProgressProcess() {
+        weak var localprocessupdateDelegate: UpdateProgress?
+        localprocessupdateDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vcprogressview) as? ViewControllerProgressProcess
+        localprocessupdateDelegate?.processTermination()
+    }
+
+    func seterrorinfo(info: String) {
+        guard info != "" else {
+            self.dryRunOrRealRun.isHidden = true
+            return
+        }
+        self.dryRunOrRealRun.textColor = .red
+        self.dryRunOrRealRun.isHidden = false
+        self.dryRunOrRealRun.stringValue = info
+    }
+
+    // Function for getting numbers out of output object updated when
+    // Process object executes the job.
+    func setNumbers(outputprocess: OutputProcess?) {
+        globalMainQueue.async(execute: { () -> Void in
+            guard outputprocess != nil else {
+                self.transferredNumber.stringValue = ""
+                self.transferredNumberSizebytes.stringValue = ""
+                self.totalNumber.stringValue = ""
+                self.totalNumberSizebytes.stringValue = ""
+                self.totalDirs.stringValue = ""
+                self.newfiles.stringValue = ""
+                self.deletefiles.stringValue = ""
+                return
+            }
+            let remoteinfotask = RemoteinfonumbersOnetask(outputprocess: outputprocess)
+            self.transferredNumber.stringValue = remoteinfotask.transferredNumber!
+            self.transferredNumberSizebytes.stringValue = remoteinfotask.transferredNumberSizebytes!
+            self.totalNumber.stringValue = remoteinfotask.totalNumber!
+            self.totalNumberSizebytes.stringValue = remoteinfotask.totalNumberSizebytes!
+            self.totalDirs.stringValue = remoteinfotask.totalDirs!
+            self.newfiles.stringValue = remoteinfotask.newfiles!
+            self.deletefiles.stringValue = remoteinfotask.deletefiles!
+        })
+    }
+
+    // Returns number set from dryrun to use in logging run
+    // after a real run. Logging is in newSingleTask object.
+    func gettransferredNumber() -> String {
+        return self.transferredNumber.stringValue
+    }
+
+    func gettransferredNumberSizebytes() -> String {
+        return self.transferredNumberSizebytes.stringValue
+    }
+}
---
+++ RsyncGUI/extensionVCMaintableviewDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  extensionVCMaintableviewDelegate.swift
+//  RsyncGUI
+//
+//  Created by Thomas Evensen on 26/08/2019.
+//  Copyright © 2019 Thomas Evensen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+extension ViewControllerMain: NSTableViewDataSource {
+    // Delegate for size of table
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.configurations?.configurationsDataSourcecount() ?? 0
+    }
+}
+
+extension ViewControllerMain: NSTableViewDelegate, Attributedestring {
+
+    // TableView delegates
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        guard self.configurations != nil else { return nil}
+        if row > self.configurations!.configurationsDataSourcecount() - 1 { return nil }
+        let object: NSDictionary = self.configurations!.getConfigurationsDataSource()![row]
+        let markdays: Bool = self.configurations!.getConfigurations()[row].markdays
+        let celltext = object[tableColumn!.identifier] as? String
+        if tableColumn!.identifier.rawValue == "daysID" {
+            if markdays {
+                return self.attributedstring(str: celltext!, color: NSColor.red, align: .right)
+            } else {
+                return object[tableColumn!.identifier] as? String
+            }
+        } else if tableColumn!.identifier.rawValue == "offsiteServerCellID",
+            ((object[tableColumn!.identifier] as? String)?.isEmpty) == true {
+            return "localhost"
+        } else if tableColumn!.identifier.rawValue == "statCellID" {
+            if row == self.index {
+                if self.singletask == nil {
+                    return #imageLiteral(resourceName: "yellow")
+                } else {
+                    return #imageLiteral(resourceName: "green")
+                }
+            }
+        } else {
+            if tableColumn!.identifier.rawValue == "batchCellID" {
+                return object[tableColumn!.identifier] as? Int
+            } else {
+                if (self.tcpconnections?.gettestAllremoteserverConnections()?[row]) ?? false && celltext != nil {
+                    return self.attributedstring(str: celltext!, color: NSColor.red, align: .left)
+                } else {
+                    return object[tableColumn!.identifier] as? String
+                }
+            }
+        }
+        return nil
+    }
+
+    // Toggling batch
+    func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
+        if self.process != nil {
+            self.abortOperations()
+        }
+        if self.configurations!.getConfigurations()[row].task == ViewControllerReference.shared.synchronize {
+            self.configurations!.enabledisablebatch(row)
+        }
+    }
+}
