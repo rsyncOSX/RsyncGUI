b5c0a64e492efe177d31c7fcdf2a1a28dfd5e19b
Thomas Evensen <thomeven@gmail.com>
2019-11-19 15:58:02 +0100

    Update ConvertSchedules.swift

--- RsyncGUI/ConvertSchedules.swift
+++ RsyncGUI/ConvertSchedules.swift
@@ -12,30 +12,31 @@ struct ConvertSchedules: SetSchedules {
 
     var schedules: [NSDictionary]?
 
-    init() {
-        var array = [NSDictionary]()
-        // Reading Schedules from memory
-        if let schedules = self.schedules?.getSchedule() {
-            for i in 0 ..< schedules.count {
-                let schedule = schedules[i]
-                let dict: NSMutableDictionary = [
-                    "hiddenID": schedule.hiddenID,
-                    "dateStart": schedule.dateStart,
-                    "schedule": schedule.schedule,
-                    "executed": schedule.logrecords,
-                    "offsiteserver": schedule.offsiteserver ?? "localhost"]
-                if schedule.dateStop != nil {
-                    dict.setValue(schedule.dateStop, forKey: "dateStop")
-                }
-                if schedule.delete ?? false == false {
-                    array.append(dict)
-                } else {
-                    if schedule.logrecords.isEmpty == false {
-                         array.append(dict)
-                    }
-                }
-            }
-        }
-        self.schedules = array
-    }
+     init() {
+           var array = [NSDictionary]()
+           // Reading Schedules from memory
+           if let schedules = self.schedules?.getSchedule() {
+               for i in 0 ..< schedules.count {
+                   let dict: NSMutableDictionary = [
+                       "hiddenID": schedules[i].hiddenID,
+                       "dateStart": schedules[i].dateStart,
+                       "schedule": schedules[i].schedule,
+                       "executed": schedules[i].logrecords,
+                       "offsiteserver": schedules[i].offsiteserver ?? "localhost"]
+                   if schedules[i].dateStop != nil {
+                       dict.setValue(schedules[i].dateStop, forKey: "dateStop")
+                   }
+                   if schedules[i].delete ?? false == false {
+                       array.append(dict)
+                   } else {
+                       if schedules[i].logrecords.isEmpty == false {
+                           if schedules[i].delete ?? false == false {
+                               array.append(dict)
+                           }
+                       }
+                   }
+               }
+           }
+           self.schedules = array
+       }
 }
