f2458c9ab4279a2ce217c95064c23ee7e4d7ae13
Thomas Evensen <thomeven@gmail.com>
2020-01-15 07:32:05 +0100

    Update Configurations.swift

--- RsyncGUI/Configurations.swift
+++ RsyncGUI/Configurations.swift
@@ -41,28 +41,28 @@ class Configurations: ReloadTable, SetSchedules {
     // Reference to check TCP-connections
     var tcpconnections: TCPconnections?
 
-    /// Function for getting the profile
+    // Function for getting the profile
     func getProfile() -> String? {
         return self.profile
     }
 
-    /// Function for getting Configurations read into memory
-    /// - parameter none: none
-    /// - returns : Array of configurations
+    // Function for getting Configurations read into memory
+    // - parameter none: none
+    // - returns : Array of configurations
     func getConfigurations() -> [Configuration] {
         return self.configurations ?? []
     }
 
-    /// Function for getting arguments for all Configurations read into memory
-    /// - parameter none: none
-    /// - returns : Array of arguments
+    // Function for getting arguments for all Configurations read into memory
+    // - parameter none: none
+    // - returns : Array of arguments
     func getargumentAllConfigurations() -> [ArgumentsOneConfiguration] {
         return self.argumentAllConfigurations ?? []
     }
 
-    /// Function for getting the number of configurations used in NSTableViews
-    /// - parameter none: none
-    /// - returns : Int
+    // Function for getting the number of configurations used in NSTableViews
+    // - parameter none: none
+    // - returns : Int
     func configurationsDataSourcecount() -> Int {
         if self.configurationsDataSource == nil {
             return 0
@@ -71,17 +71,17 @@ class Configurations: ReloadTable, SetSchedules {
         }
     }
 
-    /// Function for getting Configurations read into memory
-    /// as datasource for tableViews
-    /// - parameter none: none
-    /// - returns : Array of Configurations
+    // Function for getting Configurations read into memory
+    // as datasource for tableViews
+    // - parameter none: none
+    // - returns : Array of Configurations
     func getConfigurationsDataSource() -> [NSDictionary]? {
         return self.configurationsDataSource
     }
 
-    /// Function for getting all Configurations marked as backup (not restore)
-    /// - parameter none: none
-    /// - returns : Array of NSDictionary
+    // Function for getting all Configurations marked as backup (not restore)
+    // - parameter none: none
+    // - returns : Array of NSDictionary
     func getConfigurationsDataSourceSynchronize() -> [NSMutableDictionary]? {
         var configurations: [Configuration] = self.configurations!.filter { ($0.task == ViewControllerReference.shared.synchronize) }
         var data = [NSMutableDictionary]()
@@ -101,17 +101,17 @@ class Configurations: ReloadTable, SetSchedules {
         return data
     }
 
-    /// Function returns all Configurations marked for backup.
-    /// - returns : array of Configurations
+    // Function returns all Configurations marked for backup.
+    // - returns : array of Configurations
     func getConfigurationsBatch() -> [Configuration] {
         return self.configurations!.filter { ($0.task == ViewControllerReference.shared.synchronize) && ($0.batch == 1) }
     }
 
-    /// Function computes arguments for rsync, either arguments for
-    /// real runn or arguments for --dry-run for Configuration at selected index
-    /// - parameter index: index of Configuration
-    /// - parameter argtype : either .arg or .argdryRun (of enumtype argumentsRsync)
-    /// - returns : array of Strings holding all computed arguments
+    // Function computes arguments for rsync, either arguments for
+    // real runn or arguments for --dry-run for Configuration at selected index
+    // - parameter index: index of Configuration
+    // - parameter argtype : either .arg or .argdryRun (of enumtype argumentsRsync)
+    // - returns : array of Strings holding all computed arguments
     func arguments4rsync(index: Int, argtype: ArgumentsRsync) -> [String] {
         let allarguments = self.argumentAllConfigurations![index]
         switch argtype {
@@ -124,11 +124,11 @@ class Configurations: ReloadTable, SetSchedules {
         }
     }
 
-    /// Function computes arguments for rsync, either arguments for
-    /// real runn or arguments for --dry-run for Configuration at selected index
-    /// - parameter index: index of Configuration
-    /// - parameter argtype : either .arg or .argdryRun (of enumtype argumentsRsync)
-    /// - returns : array of Strings holding all computed arguments
+    // Function computes arguments for rsync, either arguments for
+    // real runn or arguments for --dry-run for Configuration at selected index
+    // - parameter index: index of Configuration
+    // - parameter argtype : either .arg or .argdryRun (of enumtype argumentsRsync)
+    // - returns : array of Strings holding all computed arguments
     func arguments4restore(index: Int, argtype: ArgumentsRsync) -> [String] {
         let allarguments = self.argumentAllConfigurations![index]
         switch argtype {
@@ -158,8 +158,8 @@ class Configurations: ReloadTable, SetSchedules {
         return allarguments.verify ?? []
     }
 
-    /// Function is adding new Configurations to existing in memory.
-    /// - parameter dict : new record configuration
+    // Function is adding new Configurations to existing in memory.
+    // - parameter dict : new record configuration
     func appendconfigurationstomemory(dict: NSDictionary) {
         let config = Configuration(dictionary: dict)
         self.configurations!.append(config)
@@ -180,30 +180,30 @@ class Configurations: ReloadTable, SetSchedules {
         _ = Logging(outputprocess: outputprocess)
     }
 
-    /// Function is updating Configurations in memory (by record) and
-    /// then saves updated Configurations from memory to persistent store
-    /// - parameter config: updated configuration
-    /// - parameter index: index to Configuration to replace by config
+    // Function is updating Configurations in memory (by record) and
+    // then saves updated Configurations from memory to persistent store
+    // - parameter config: updated configuration
+    // - parameter index: index to Configuration to replace by config
     func updateConfigurations(_ config: Configuration, index: Int) {
         self.configurations![index] = config
         _ = PersistentStorageConfiguration(profile: self.profile).saveconfigInMemoryToPersistentStore()
     }
 
-    /// Function deletes Configuration in memory at hiddenID and
-    /// then saves updated Configurations from memory to persistent store.
-    /// Function computes index by hiddenID.
-    /// - parameter hiddenID: hiddenID which is unique for every Configuration
+    // Function deletes Configuration in memory at hiddenID and
+    // then saves updated Configurations from memory to persistent store.
+    // Function computes index by hiddenID.
+    // - parameter hiddenID: hiddenID which is unique for every Configuration
     func deleteConfigurationsByhiddenID(hiddenID: Int) {
         let index = self.getIndex(hiddenID: hiddenID)
         self.configurations!.remove(at: index)
         _ = PersistentStorageConfiguration(profile: self.profile).saveconfigInMemoryToPersistentStore()
     }
 
-    /// Function toggles Configurations for batch or no
-    /// batch. Function updates Configuration in memory
-    /// and stores Configuration i memory to
-    /// persisten store
-    /// - parameter index: index of Configuration to toogle batch on/off
+    // Function toggles Configurations for batch or no
+    // batch. Function updates Configuration in memory
+    // and stores Configuration i memory to
+    // persisten store
+    // - parameter index: index of Configuration to toogle batch on/off
     func togglebatch(_ index: Int) {
         if self.configurations![index].batch == 1 {
             self.configurations![index].batch = 0
@@ -214,9 +214,9 @@ class Configurations: ReloadTable, SetSchedules {
         self.reloadtable(vcontroller: .vctabmain)
     }
 
-    /// Function return the reference to object holding data and methods
-    /// for batch execution of Configurations.
-    /// - returns : reference to to object holding data and methods
+    // Function return the reference to object holding data and methods
+    // for batch execution of Configurations.
+    // - returns : reference to to object holding data and methods
     func getbatchQueue() -> BatchTaskWorkQueu? {
         if self.batchQueue == nil {
             self.batchQueue = BatchTaskWorkQueu(configurations: self)
@@ -224,8 +224,8 @@ class Configurations: ReloadTable, SetSchedules {
         return self.batchQueue
     }
 
-    /// Function is getting the number of rows batchDataQueue
-    /// - returns : the number of rows
+    // Function is getting the number of rows batchDataQueue
+    // - returns : the number of rows
     func batchQueuecount() -> Int {
         return self.batchQueue?.getbatchtaskstodocount() ?? 0
     }
@@ -330,11 +330,11 @@ class Configurations: ReloadTable, SetSchedules {
         }
     }
 
-    /// Function is reading all Configurations into memory from permanent store and
-    /// prepare all arguments for rsync. All configurations are stored in the private
-    /// variable within object.
-    /// Function is destroying any previous Configurations before loading new and computing new arguments.
-    /// - parameter none: none
+    // Function is reading all Configurations into memory from permanent store and
+    // prepare all arguments for rsync. All configurations are stored in the private
+    // variable within object.
+    // Function is destroying any previous Configurations before loading new and computing new arguments.
+    // - parameter none: none
     func readconfigurations() {
         self.argumentAllConfigurations = [ArgumentsOneConfiguration]()
         let store: [Configuration]? = PersistentStorageConfiguration(profile: self.profile).getConfigurations()
@@ -343,6 +343,8 @@ class Configurations: ReloadTable, SetSchedules {
                 self.configurations?.append(store![i])
                 let rsyncArgumentsOneConfig = ArgumentsOneConfiguration(config: store![i])
                 self.argumentAllConfigurations!.append(rsyncArgumentsOneConfig)
+                // Sequrity scoped URLS
+                self.appendsequrityscopedurls(config: store![i])
             }
         }
         // Then prepare the datasource for use in tableviews as Dictionarys
