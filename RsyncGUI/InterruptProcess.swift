0dd12603c3dfebb93aeda3eeca477c3cb285c634
Thomas Evensen <thomeven@gmail.com>
2020-07-17 08:27:32 +0200

    Updates

--- RsyncGUI/InterruptProcess.swift
+++ RsyncGUI/InterruptProcess.swift
@@ -5,11 +5,16 @@
 //  Created by Thomas Evensen on 18/06/2020.
 //  Copyright © 2020 Thomas Evensen. All rights reserved.
 //
+// swiftlint:disable line_length
 
 import Foundation
 
 struct InterruptProcess {
+    // Enable and disable select profile
+    weak var profilepopupDelegate: DisableEnablePopupSelectProfile?
     init() {
+        self.profilepopupDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
+        self.profilepopupDelegate?.enableselectpopupprofile()
         guard ViewControllerReference.shared.process != nil else { return }
         let output = OutputProcess()
         let formatter = DateFormatter()
@@ -22,6 +27,8 @@ struct InterruptProcess {
     }
 
     init(output: OutputProcess?) {
+        self.profilepopupDelegate = ViewControllerReference.shared.getvcref(viewcontroller: .vctabmain) as? ViewControllerMain
+        self.profilepopupDelegate?.enableselectpopupprofile()
         guard ViewControllerReference.shared.process != nil, output != nil else { return }
         _ = Logging(output, true)
         ViewControllerReference.shared.process?.interrupt()
