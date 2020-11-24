0a4bd21f5227881f40c0201f8672c93e4e5a4d2c
Thomas Evensen <thomeven@gmail.com>
2020-11-24 08:23:47 +0100

    update

--- RsyncGUI/PersistentStorageAssist.swift
+++ RsyncGUI/PersistentStorageAssist.swift
@@ -6,15 +6,34 @@
 //  Copyright © 2020 Thomas Evensen. All rights reserved.
 //
 
+import Files
 import Foundation
 
 final class PersistentStorageAssist: ReadWriteDictionary {
-    var assist: [Set<String>]?
+    var assistsets: [Set<String>]?
 
     // Save assist configuration
     func saveassist() {
-        if let array: [NSDictionary] = ConvertAssist(assistassets: self.assist).assist {
-            self.writeToStore(array: array)
+        let root = NamesandPaths(profileorsshrootpath: .profileroot)
+        if let atpath = root.fullroot {
+            do {
+                if try Folder(path: atpath).containsFile(named: ViewControllerReference.shared.assistplist) {
+                    let question: String = NSLocalizedString("PLIST file exists: ", comment: "Logg")
+                    let text: String = NSLocalizedString("Cancel or Save", comment: "Logg")
+                    let dialog: String = NSLocalizedString("Save", comment: "Logg")
+                    let answer = Alerts.dialogOrCancel(question: question + " " +
+                        ViewControllerReference.shared.assistplist, text: text, dialog: dialog)
+                    if answer {
+                        if let array: [NSDictionary] = ConvertAssist(assistassets: self.assistsets).assist {
+                            self.writeToStore(array: array)
+                        }
+                    }
+                } else {
+                    if let array: [NSDictionary] = ConvertAssist(assistassets: self.assistsets).assist {
+                        self.writeToStore(array: array)
+                    }
+                }
+            } catch {}
         }
     }
 
@@ -30,8 +49,15 @@ final class PersistentStorageAssist: ReadWriteDictionary {
         self.writeNSDictionaryToPersistentStorage(array: array)
     }
 
-    init(assistassets: [Set<String>]?) {
+    init(assist: Assist?) {
         super.init(profile: nil, whattoreadwrite: .assist)
-        self.assist = assistassets
+        if let assist = assist {
+            self.assistsets = [Set]()
+            if let remotecomputers = assist.remotecomputers { self.assistsets?.append(remotecomputers) }
+            if let remoteusers = assist.remoteusers { self.assistsets?.append(remoteusers) }
+            if let remotehome = assist.remotehome { self.assistsets?.append(remotehome) }
+            if let catalogs = assist.catalogs { self.assistsets?.append(catalogs) }
+            if let localhome = assist.localhome { self.assistsets?.append(localhome) }
+        }
     }
 }
