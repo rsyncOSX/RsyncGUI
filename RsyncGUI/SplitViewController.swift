e78d7adabc7e239f387abbc67cdc5ae176dcca21
Thomas Evensen <thomeven@gmail.com>
2020-12-06 12:53:07 +0100

    update

---
+++ RsyncGUI/SplitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SplitViewController.swift
+//  RsyncOSX
+//
+//  Created by Thomas Evensen on 01/12/2020.
+//  Copyright © 2020 Thomas Evensen. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class SplitViewController: NSSplitViewController {
+    /*
+     private var actionviewcontrollersidebar: ViewControllerSideBar?
+     private var tabviewcontroller: TabViewController?
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         self.actionviewcontrollersidebar = self.splitViewItems[0].viewController as? ViewControllerSideBar
+         self.tabviewcontroller = self.splitViewItems[1].viewController as? TabViewController
+     }
+     */
+}
+
+/*
+ extension SplitViewController {
+     override func splitView(_: NSSplitView, canCollapseSubview _: NSView) -> Bool {
+         return false
+     }
+
+     override func splitViewDidResizeSubviews(_: Notification) {}
+ }
+ */
